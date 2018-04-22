52606d239347d1ec6e3e2acbce734bdf10c1259c
Shawfe Sung <shawfe@subroutine.name>
2018-04-22 17:19:22 -0400

    Added option to solver puzzle in UI

--- src/main/resources/static/trainer.controller.puzzler.js
+++ src/main/resources/static/trainer.controller.puzzler.js
@@ -1,6 +1,37 @@
 angular.module('gasogame24trainer')
 .controller('game24puzzler', function($scope, $http) {
+    function PuzzleNumber( number ) {
+        this.numberHistory = [number];
+        this.used = false;
+
+        this.use = function(){
+            this.used = true;
+        }
+        this.unuse = function(){
+            this.used = false;
+        }
+        this.update = function( n ){
+            this.numberHistory.push( n );
+        }
+        this.clearHistory = function(){
+            this.numberHistory = [this.numberHistory[0]];
+        }
+    }
     const none = 999;
+
+    $scope.numbers = [];
+    $scope.selectedNumber = none;
+
+    $scope.operators = [ "+", "-", "x", "\xf7" ];
+    $scope.selectedOperator = none;
+
+    $scope.setNumbers = function( numbers ) {
+        $scope.numbers = [];
+        numbers.forEach( function( n ) {
+            const nn = new PuzzleNumber( n );
+            $scope.numbers.push( nn );
+        } );
+    }
 
     $scope.anotherPuzzle = function( difficultyRank ) {
         if( difficultyRank ){
@@ -19,19 +50,17 @@ angular.module('gasogame24trainer')
                         $scope.numbers = response.data.puzzle.numbers;
                     },
                     function(error){
-                        $scope.numbers = [2, 22, 11, 9];
+                        //$scope.numbers = [2, 22, 11, 9];
+                        $scope.setNumbers( [2, 22, 11, 9] );
                     }
                 )
         }
     }
 
     $scope.anotherPuzzle();
-    $scope.operators = [ "+", "-", "x", "&divide;" ];
-    $scope.selectedNumber = none;
-    $scope.selectedOperator = none;
 
     $scope.numberClass = function( position ){
-        const number = $scope.numbers[position];
+        const number = $scope.numberAt( position );
         const result = [];
         if( number === 9 ){
             result.push( "nine" );
@@ -101,57 +130,106 @@ angular.module('gasogame24trainer')
         $scope.isSolutionHidden = true
     }
 
-    $scope.clicked = [];
-    $scope.selectNumber = function( n ) {
+    $scope.answerGrid = [];
+    $scope.selectNumber = function( n ){
         if( $scope.selectedOperator === none ){
-            if( $scope.selectedNumber !== n ){
+            if( !$scope.numbers[n].used ){
                 $scope.selectedNumber = n;
-                $scope.clicked.push( $scope.numbers[n] );
             }
         }
         else{
             if( $scope.selectedNumber !== n ){
-                const prevNumber = $scope.numbers[$scope.selectedNumber];
-                const currNumber = $scope.numbers[n];
-                if( $scope.selectedOperator === 0 ){
-                    // done with one operation, so clear operator
-                    $scope.selectedOperator = none;
-                    // calculate result of this operation
-                    const result = prevNumber + currNumber;
-                    // set current number position to result
-                    $scope.numbers[n] = result;
-                    // and clear the other number
-                    $scope.numbers[selectedNumber] = "";
+                const prevNumber = $scope.numberAt( $scope.selectedNumber );
+                const currNumber = $scope.numberAt( n );
 
-                    //$scope.selectedNumber = n;
-                    $scope.clicked.push( currNumber );
-                    $scope.clicked.push( "=" );
-                    $scope.clicked.push( result );
+                // done with one operation, so clear operator
+                // calculate result of this operation
+                let result;
+                if( $scope.selectedOperator === 0 ){
+                    result = prevNumber + currNumber;
                 }
+                else if( $scope.selectedOperator === 1 ){
+                    result = prevNumber - currNumber;
+                }
+                else if( $scope.selectedOperator === 2 ){
+                    result = prevNumber * currNumber;
+                }
+                else if( $scope.selectedOperator === 3 ){
+                    result = prevNumber / currNumber;
+                }
+
+                // set current number position to result
+                $scope.numbers[n].update( result );
+                // and clear the other number
+                //$scope.setNumber( $scope.selectedNumber, "\xa0" );
+                $scope.numbers[$scope.selectedNumber].use();
+
+                $scope.answerGrid.push( prevNumber );
+                $scope.answerGrid.push( $scope.operators[$scope.selectedOperator] );
+                $scope.answerGrid.push( currNumber );
+                $scope.answerGrid.push( result );
+
+                $scope.selectedNumber = n;
+                $scope.selectedOperator = none;
             }
         }
     }
 
-    $scope.selectOperator = function( n ) {
+    $scope.selectOperator = function( n ){
         if( $scope.selectedNumber === none ){
             return;
         }
         if( $scope.selectedOperator !== n ){
             $scope.selectedOperator = n;
-            $scope.clicked.push( $scope.operators[n] );
         }
     }
 
-    $scope.equation = function( n ) {
-//        let result = "";
-//        for( int i = 0; i < 5; i = i + 1 ){
-//            if( $scope.clicked[i] ){
-//                if( i > 0 ){
-//                    result += " ";
-//                }
-//                result += $scope.clicked[i];
-//            }
-//        }
-//        return result;
+    $scope.equation = function( n ){
+        let result = "";
+        for( let i = 0; i < 5; i = i + 1 ){
+            if( $scope.answerGrid[i] ){
+                if( i > 0 ){
+                    result += "\xa0";
+                }
+                result += $scope.answerGrid[i];
+            }
+        }
+        return result;
+    }
+
+    $scope.answerGridValue = function( eq, pos ){
+        const index = eq * 4 + pos;
+        if( $scope.answerGrid.length < index + 1 ){
+            return "\xa0";
+        }
+        else{
+            return $scope.answerGrid[index];
+        }
+    }
+
+    $scope.numberAt = function( p ){
+        if( !$scope.numbers ){
+            return "\xa0";
+        }
+        if( $scope.numbers.length < p + 1 ){
+            return "\xa0";
+        }
+        if( $scope.numbers[p].used ){
+            return "\xa0";
+        }
+
+        // will fail if there is no number, but that should not happen
+        return $scope.numbers[p].numberHistory.slice( -1 )[0];
+    }
+
+    $scope.selectBackspace = function(){
+        for( let i = 0; i < $scope.numbers.length; i = i + 1 ){
+            $scope.numbers[i].unuse();
+            $scope.numbers[i].clearHistory();
+        }
+
+        $scope.answerGrid = [];
+        $scope.selectedNumber = none;
+        $scope.selectedOperator = none;
     }
 })
